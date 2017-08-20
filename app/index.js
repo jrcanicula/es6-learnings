3295aab7b0725193b35ada6f9c04187f463ca78d
jrcanicula <jaime.canicula@gmail.com>
2017-08-20 15:21:38 +0800

    arrow function part 2

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+/*arrow function*/
+
 function blastoff(){
 	console.log('3...32....23...123.');
 }
@@ -11,3 +13,22 @@ setTimeout(() => blastoffs('c','b'),1000);
 
 
 blastoffs('a','5');
+
+
+this.a  = 25;
+
+/*
+let print = function(){
+	this.a = 50;
+	console.log('this.a '.this.a);
+}
+
+print();
+
+*/ console.log('asd');
+
+console.log(this.a);
+ 
+let arrowPrint  = () => console.log(this.a);
+
+arrowPrint();
