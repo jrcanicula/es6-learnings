3f6227503f1a1dbf917c82de974772616c57c3b8
jrcanicula <jaime.canicula@gmail.com>
2017-08-20 15:14:20 +0800

    Function Part 1

--- app/index.js
+++ app/index.js
@@ -1,22 +1,13 @@
-//let wizard = {magical : true,power : 10};
-
-//let magical = wizard.magical;
-//let power = wizard.power;
-
-//let {magical, power} = wizard;
-
-//console.log(magical,power);
+function blastoff(){
+	console.log('3...32....23...123.');
+}
 
 
-/*part 2*/
 
-let magical = true;
-let power = 2;
+const blastoffs = (b,c) => console.log(`i love pia ${b} haha, ${c}`,b,c);
+//blastoff();
 
-console.log(magical,power);
+setTimeout(() => blastoffs('c','b'),1000);
 
-let ranger = {magical: false, power: 9};
 
-({magical,power} = ranger);
-
-console.log(magical,power);
+blastoffs('a','5');
